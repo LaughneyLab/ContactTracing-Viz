40fd49f95a0ae19a6b2248c347291f7d41978b2e
Austin Varela <austinv11@gmail.com>
2023-07-17 12:01:20 -0400

    Use accordion instead of tabs on the home page

--- pages/home.py
+++ pages/home.py
@@ -17,23 +17,28 @@ layout = [
         "ContactTracing Interactive Visualizer",
         "This is a web application that allows for ContactTracing outputs to be easily interpreted.",
         dbc.Row([
-            dbc.Container(dbc.Container([
-                dbc.Card([
-                    dbc.CardHeader(
-                        dbc.Tabs([
-                            dbc.Tab(label='Welcome', tab_id='default', labelClassName="text-muted"),
-                            dbc.Tab(label='Dataset Information', tab_id='dataset', labelClassName="text-muted"),
-                            dbc.Tab(label='Visualization Information', tab_id='plot', labelClassName="text-muted"),
-                            dbc.Tab(label='Miscellaneous', tab_id='misc', labelClassName="text-muted"),
-                        ], id='home-tabs', active_tab='default', persistence=False)
-                    ),
-                    dbc.CardBody([
-                        html.H4(id='home-tab-title', className='card-title'),
-                        html.Hr(),
-                        html.P(id='home-tab-content', className='card-text'),
-                    ])
-                ], color="information", inverse=False),
-            ]), id='main-content-container', fluid=True)
+            dbc.Accordion([
+                dbc.AccordionItem([
+                    html.H4([html.I("ContactTracing"), ", studying the impact of chromosomal instability (CIN) & STING on the tumor ecosystem."], className='card-title'),
+                    html.Hr(),
+                    html.P(home_welcome_info(), className='card-text'),
+                ], title='Welcome'),
+                dbc.AccordionItem([
+                    html.H4([html.I("ContactTracing"), " is a unique approach for profiling cellular responses to the tumor microenvironment."], className='card-title'),
+                    html.Hr(),
+                    html.P(home_dataset_descriptions(), className='card-text'),
+                ], title='Dataset Information'),
+                dbc.AccordionItem([
+                    html.H4([html.I("ContactTracing"), " produces information-rich data on condition-specific genetic responses."], className='card-title'),
+                    html.Hr(),
+                    html.P(home_plot_descriptions(), className='card-text'),
+                ], title='Visualization Information'),
+                dbc.AccordionItem([
+                    html.H4(['Additional information on ', html.I("ContactTracing"), ' and this website.'], className='card-title'),
+                    html.Hr(),
+                    html.P(home_misc_info(), className='card-text'),
+                ], title='Miscellaneous'),
+            ], active_item=['item-0', 'item-1', 'item-2', 'item-3'], always_open=True)
         ])
     ),
 ]
@@ -68,27 +73,5 @@ def show_offcanvas_content(circos_n_clicks, celltype_n_clicks, ligand_effect_n_c
         return False, celltype_n_clicks > 0, False
     elif triggered == "full-ligand-effect-help-button":
         return False, False, ligand_effect_n_clicks > 0
-    else:
-        raise PreventUpdate
-
-
-@callback(
-    Output('home-tab-title', 'children'),
-    Output('home-tab-content', 'children'),
-    Input('home-tabs', 'active_tab')
-)
-def update_tab_content(active_tab):
-    if active_tab == 'default':
-        return [html.I("ContactTracing"), ", studying the impact of chromosomal instability (CIN) & STING on the tumor ecosystem."], \
-            home_welcome_info()
-    elif active_tab == 'dataset':
-        return [html.I("ContactTracing"), " is a unique approach for profiling cellular responses to the tumor microenvironment."], \
-            home_dataset_descriptions()
-    elif active_tab == 'plot':
-        return [html.I("ContactTracing"), " produces information-rich data on condition-specific genetic responses."], \
-            home_plot_descriptions()
-    elif active_tab == 'misc':
-        return ['Additional information on ', html.I("ContactTracing"), ' and this website.'], \
-            home_misc_info()
     else:
         raise PreventUpdate
